1ab6ab6abf436baba6a92912b39d5ce0d756e6ed
Viktor Rudi <viktor@Rudey.local>
2019-06-26 14:52:26 +0200

    started on routes

--- view/src/App.js
+++ view/src/App.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react'
+import { BrowserRouter, Route } from 'react-router-dom'
 import TodoApp from './components/TodoApp/TodoApp'
+import Login from './components/Login'
 import TodoProvider from './TodoContext'
 
 class App extends Component {
   render () {
     return (
-      <TodoProvider>
-        <TodoApp />
-      </TodoProvider>
+      <BrowserRouter>
+        <Route path="/login" component={Login} />
+        <TodoProvider>
+          <Route path="/todo" component={TodoApp} />
+        </TodoProvider>
+      </BrowserRouter>
     )
   }
 }
---
+++ view/src/components/Login.js
@@ -0,0 +1,6 @@
+import React, { useContext } from 'react'
+import Loader from './Loader/Loader'
+
+export default function Login () {
+  return <div>login</div>
+}
