8006c0dec7397f43a310ed7f124f3238a0b438d5
Viktor Rudi <viktor@Rudey.local>
2019-07-06 21:27:20 +0200

    added settings button

--- frontend/src/components/TodoApp/UserOptions/UserOptions.js
+++ frontend/src/components/TodoApp/UserOptions/UserOptions.js
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react'
 import { AppContext } from '../../../AppContext'
-import { MdExitToApp } from 'react-icons/md'
+import { MdExitToApp, MdSettings } from 'react-icons/md'
 
 import './UserOptions.scss'
 
@@ -8,8 +8,17 @@ export default function UserFooter () {
   const context = useContext(AppContext)
   return (
     <footer className="UserOptions">
-      <span className="UserOptions__option" onClick={() => context.logOut()}>
+      <span
+        className="UserOptions__option logout"
+        onClick={() => context.logOut()}
+      >
         <MdExitToApp /> Log out
+      </span>
+      <span
+        className="UserOptions__option settings"
+        // onClick={}
+      >
+        <MdSettings /> Settings
       </span>
     </footer>
   )
