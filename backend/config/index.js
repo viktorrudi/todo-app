02f77800863dcaec56a80c561301b917383bd902
Viktor Rudi <vrudi91@gmail.com>
2019-07-23 15:46:51 +0200

    Update index.js
    put secret in .env

--- backend/config/index.js
+++ backend/config/index.js
@@ -11,7 +11,7 @@ module.exports = {
     uri: process.env.DB_CONNECTION,
   },
   auth: {
-    secret: 'secretKey',
+    secret: process.env.AUTH_SECRET,
     expireTime: '1h',
     saltRounds: 10,
   },
